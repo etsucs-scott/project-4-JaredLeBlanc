3074efb93cae6d7b7deaa1e0ddb7d7110836c03d
Jared LeBlanc <LEBLANCJ@ETSU.EDU>
2026-04-27 14:18:37 -0400

    switching to Avalonia

---
+++ SkyjoAvaloniaApp/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SkyjoAvaloniaApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
